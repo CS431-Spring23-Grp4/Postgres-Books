bc2ec69cfa75e3e409fc140d98645c04a680a6e1
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2023-04-18 19:27:32 -0700

    Insertion for series and genre tables

--- insert.sql
+++ insert.sql
@@ -1,2 +1,17 @@
 -- Set future types to affect group4 schema by default 
 SET search_path TO group4, public;
+
+-- Values for genre table
+insert into genre (name) values 
+('Fantasy'),
+('Technology'),
+('Political Satire'),
+('Science Fiction'),
+('Fiction'),
+('Romance');
+
+-- Values for series table
+insert into series (title) values
+('Harry Potter'),
+('The Hunger Games'),
+('Folger Shakespeare Library');
