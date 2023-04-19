4d42c8aef4189d1cea18a18c1d35237a5a849416
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2023-04-19 13:03:25 -0700

    Insertion for book_author

--- insert.sql
+++ insert.sql
@@ -15,3 +15,13 @@ insert into series (title) values
 ('Harry Potter'),
 ('The Hunger Games'),
 ('Folger Shakespeare Library');
+
+-- Values for series table
+insert into book_author (isbn, author_id) values
+('9780747532699', 1),
+('074754624X', 1),
+('9780135957059', 5),
+('9780135957059', 6),
+('9780451526342', 4),
+('9780553214208', 2),
+('439023483', 3);
