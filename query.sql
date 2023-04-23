9f14b70eb12fab9b5f6b311b1cb9c3c69fe35f6c
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2023-04-22 22:20:11 -0700

    Fix query numbers, remove inner join and use where clause instead

--- query.sql
+++ query.sql
@@ -6,25 +6,17 @@ select distinct a.first_name, a.last_name
 from author a, book b, book_author ba 
 where b.series_id > 1 and b.isbn = ba.isbn and ba.author_id = a.author_id;
 
---2.) List the Name of Publishers of Books which are in the Technology genre
-select p.name 
-from publisher p
-inner join book b on b.publisher_id = p.publisher_id 
-inner join book_genre bg on bg.isbn = b.isbn 
-inner join genre g on g.genre_id = bg.genre_id 
-where g."name" = 'Technology';
+--5.) List the Name of Publishers of Books which are in the Technology genre
+select distinct p.name 
+from publisher p, book b, book_genre bg, genre g 
+where p.publisher_id = b.publisher_id and b.isbn = bg.isbn and bg.genre_id = g.genre_id and g.name = 'Technology';
 
---3.) List the First Name and Last Name of Authors of Political Satire Books
-select a.first_name, a.last_name 
-from author a 
-inner join book_author ba on ba.author_id = a.author_id 
-inner join book_genre bg on bg.isbn = ba.isbn 
-inner join genre g on g.genre_id = bg.genre_id 
-where g."name" = 'Political Satire';
+--6.) List the First Name and Last Name of Authors of Political Satire Books
+select distinct a.first_name, a.last_name 
+from author a, book_author ba, book_genre bg, genre g 
+where a.author_id = ba.author_id and ba.isbn = bg.isbn and bg.genre_id = g.genre_id and g.name = 'Political Satire';
 
---4.) List the Title of Books with more than 200 Pages that are in the Fantasy genre
-select b.title 
-from book b 
-inner join book_genre bg on bg.isbn = b.isbn 
-inner join genre g on g.genre_id = bg.genre_id 
-where b.num_of_pages > 200 and g."name" = 'Fantasy';
+--7.) List the Title of Books with more than 200 Pages that are in the Fantasy genre
+select distinct b.title 
+from book b, book_genre bg, genre g 
+where b.isbn = bg.isbn and bg.genre_id = g.genre_id and b.num_of_pages > 200 and g.name = 'Fantasy';
